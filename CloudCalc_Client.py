69edf78133d64b66904229fc072296931fec0c2a
joseph-breitner <46498860+joseph-breitner@users.noreply.github.com>
2019-01-08 13:57:21 -0500

    Update CloudCalc_Client.py

--- CloudCalc_Client.py
+++ CloudCalc_Client.py
@@ -1,10 +1,11 @@
-#client
-#Team names: Austin Shammas and Joseph Breitner
-#Program functions correctly and is interpreted correctly
-#produces correct results
+# Client
+# Written By: Austin Shammas and Joseph Breitner
+# Program functions correctly and is interpreted correctly
+# Produces correct results
+
 import socket
 
-HOST = '127.0.0.1'
+HOST = ''
 PORT = 8889  # Arbitrary non-privileged port
 size = 1024
 
@@ -16,19 +17,21 @@ clientSocket.connect((HOST, PORT))
 
 while 1:
     receivedMessage = clientSocket.recv(1024)  # Get the message from the Server
-    print receivedMessage  # Print the Message on the screen
+    print receivedMessage                      # Print the Message on the screen
 
-    if (receivedMessage.find('enter') >= 0):  # If there is a need for input from user
-        inputFromKeyboard = raw_input()  # Get the input from the keyboard
-        clientSocket.send(inputFromKeyboard)  # and send it to the Server
-        if (inputFromKeyboard == 'exit'):  # If the input is 'exit', close the client
+    if (receivedMessage.find('enter') >= 0):   # If there is a need for input from user
+        inputFromKeyboard = raw_input()        # Get the input from the keyboard
+        clientSocket.send(inputFromKeyboard)   # and send it to the Server
+        
+        if (inputFromKeyboard == 'exit'):      # If the input is 'exit', close the client
             break
-        elif (inputFromKeyboard == 'OutputFile'): # if user enters 'OutputFile'
+        
+        elif (inputFromKeyboard == 'OutputFile'):  # if user enters 'OutputFile'
             print("Type the file name you want to display and hit enter")
-            output_name = raw_input()	# take input from user
-            fileToOpen = open(output_name, 'r')	# open file
-            openFile = fileToOpen.read().split()	# split file into array
-            fileN = "\n".join(openFile)	# join array into string
-            print(fileN)	# print the file
+            output_name = raw_input()	           # take input from user
+            fileToOpen = open(output_name, 'r')	   # open file
+            openFile = fileToOpen.read().split()   # split file into array
+            fileN = "\n".join(openFile)	           # join array into string
+            print(fileN)	                       # print the file
 
 clientSocket.close()
